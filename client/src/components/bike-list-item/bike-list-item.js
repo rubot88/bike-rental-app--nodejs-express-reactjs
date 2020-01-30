56244fd8e6952a3849ec7955ec1bcfbc4e566269
Rustam Botirov <rustambotirov88@gmail.com>
2020-01-31 00:15:37 +0200

    add animation

--- client/src/components/bike-list-item/bike-list-item.js
+++ client/src/components/bike-list-item/bike-list-item.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import { bikeTypes } from '../../utils/bikeTypes';
+
 
 const BikeListItem = ({ bike }) => {
     const { title, type, price } = bike;
     return (
-        <p>{title} / {type} / {parseFloat(price).toFixed(2)}&nbsp;UAH</p>
+        <p>{title} / {bikeTypes[type]} / ${parseFloat(price).toFixed(2)}</p>
     )
 };
 
-BikeListItem.propTypes={
+BikeListItem.propTypes = {
     bike: PropTypes.object
 }
 
--- client/src/components/bike-list/bike-list.js
+++ client/src/components/bike-list/bike-list.js
@@ -1,8 +1,10 @@
 import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 import BikeListItem from '../bike-list-item/bike-list-item';
 import BikeServiceContext from '../../context/bikeService/bikeServiceContext';
+import './bike-list.scss';
 
 
 const BikeList = ({ bikes, rented }) => {
@@ -14,21 +16,31 @@ const BikeList = ({ bikes, rented }) => {
             <button onClick={() => toggleBikeStatus(id)} type="button" className="btn btn-danger">Сдать</button> :
             <><button onClick={() => toggleBikeStatus(id)} type="button" className="btn btn-primary mr-2">Арендовать</button>
                 <button onClick={() => removeBike(id)} type="button" className="btn btn-danger">Удалить</button></>;
-        return (<li key={id}
-            className="list-group-item p-4 mb-3 d-flex justify-content-between align-items-end">
-            <BikeListItem bike={bike} />
-            <div>
-                {buttons}
-            </div>
-        </li>);
+        return (
+            <CSSTransition
+                key={id}
+                timeout={500}
+                classNames={'bike'}>
+                <li
+                    className="list-group-item p-4 mb-3 d-flex justify-content-between align-items-end">
+                    <BikeListItem bike={bike} />
+                    <div>
+                        {buttons}
+                    </div>
+                </li>
+            </CSSTransition>
+
+        );
     });
     return (
         bikesList.length ?
-            <ul className="item-list list-group mb-5" >
-                {bikesList}
-            </ul> :
-            <p>Нет велосипедов в этом списке</p>
+            <TransitionGroup
+                component="ul"
+                className=" list-group mb-5">
 
+                {bikesList}
+            </TransitionGroup> :
+            <p>Нет велосипедов в этом списке</p>
     )
 };
 
